774ea5185e9d5d6e0d836921ca1ef5b9c64174f6
Arman Ashrafian <ashy63@gmail.com>
2018-01-25 22:00:58 -0800

    static files

--- main.go
+++ main.go
@@ -1,22 +1,37 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	port := os.Getenv("PORT")
 
 	if port == "" {
-		port = "8080"
+		port = "3000" // gin port
 	}
 
-	http.HandleFunc("/", homeHandler)
-	http.ListenAndServe(":"+port, nil)
+	// Mux Router
+	r := mux.NewRouter()
+
+	// serve static files
+	r.PathPrefix("/static/").
+		Handler(http.StripPrefix("/static/",
+			http.FileServer(http.Dir("static"))))
+
+	// routes
+	r.HandleFunc("/", homeHandler)
+
+	log.Fatal(http.ListenAndServe(":"+port, r))
 
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>HELLO</h1>"))
+	t, _ := template.ParseFiles("templates/index.html")
+	t.Execute(w, nil)
 }
