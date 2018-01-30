b5aeb1d3f1764560b720e265455e81125d354a5e
Arman Ashrafian <ashy63@gmail.com>
2018-01-29 22:43:21 -0800

    cache home template

--- main.go
+++ main.go
@@ -9,12 +9,19 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// base template
+var hometemplate *template.Template
+
 func main() {
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		port = "3000" // gin port
 	}
+
+	// cache base template
+	hometemplate = template.Must(template.ParseFiles("./templates/base.html",
+		"./templates/home.html"))
 
 	// Mux Router
 	r := mux.NewRouter()
@@ -28,29 +35,26 @@ func main() {
 	r.HandleFunc("/", homeHandler)
 	r.HandleFunc("/blog/{blogid}", blogHandler)
 
+	// serve
 	log.Fatal(http.ListenAndServe(":"+port, r))
 
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles(
-		"templates/base.html",
-		"templates/home.html",
-	)
-	t.ExecuteTemplate(w, "base", "")
+	hometemplate.ExecuteTemplate(w, "base", "")
 }
 
 func blogHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	t, err := template.ParseFiles(
-		"templates/base.html",
-		"templates/blogs/"+vars["blogid"]+".html",
-	)
+	id := mux.Vars(r)["blogid"]
+
+	t, err := template.ParseFiles("./templates/base.html",
+		"./templates/blogs/blog"+id+".html")
 
 	if err != nil {
 		// go home
 		http.Redirect(w, r, "/", 301)
 		return
 	}
+
 	t.ExecuteTemplate(w, "base", "")
 }
